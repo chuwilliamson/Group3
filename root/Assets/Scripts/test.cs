ab63c9b9beef029748df4f1f5311fa63b5c98f6a
chuwilliamson <chuwilliamson@gmail.com>
2015-05-13 18:48:34 -0500

    animator for flame character
    animator setup for the flame character

---
+++ root/Assets/Scripts/test.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+    public Animator _anim;
+    void Awake()
+    {
+        
+    }
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _anim.SetTrigger("TakeDamage");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _anim.SetTrigger("DoubleFireBall");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _anim.SetTrigger("ded");
+        }
+	
+	}
+}
