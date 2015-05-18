e39030e273635302da5d9f62719b8f4b0276075a
KyleMach <mach.kyle@yahoo.com>
2015-05-18 17:10:36 -0500

    Cont Sounds
    Got the sounds to play, need to find more files to work for the needed
    slots.

--- root/Assets/Scripts/DataScripts/AudioManager.cs
+++ root/Assets/Scripts/DataScripts/AudioManager.cs
@@ -1,21 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class AudioManager : MonoBehaviour {
-	
-	public AudioClip[] Sounds;
+public class AudioManager : Singleton<AudioManager> 
+{
+	public List<AudioClip> _sounds = new List<AudioClip> ();
+	private Dictionary<string, AudioClip> soundDict = new Dictionary<string, AudioClip> ();
+	private AudioSource aSource;
 	//Need to make a way to switch between the states of music
 	//Need them in audio zone so you can hear it for certain things
 	//		ie (Enemy attack, enemy death, portal(when activited))
 	//switching between states for background, death, and powerup
 	//have a state for when hes idle and walking
 	// Use this for initialization
-	void Start () {
-	
+	void Awake()
+	{
+		foreach (AudioClip a in _sounds) 
+		{
+			soundDict.Add(a.name, a);
+		}
+		aSource = GetComponent<AudioSource> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+
+	public void PlaySound(string sound)
+	{
+
+		if (soundDict.ContainsKey (sound)) {
+			Debug.Log ("playing that sound " + sound);
+			aSource.clip = soundDict [sound];
+			aSource.Play ();
+
+		} else {
+			print ("couldn't find the sound");
+		}
 	}
 }
