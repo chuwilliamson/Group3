2cbba6d275f43b879fa9f5fa37e23582f7c7720c
KyleMach <mach.kyle@yahoo.com>
2015-05-18 09:35:12 -0500

    Sound Change

--- Assets/Scripts/DataScripts/AudioManager.cs
+++ Assets/Scripts/DataScripts/AudioManager.cs
@@ -6,7 +6,7 @@ public class AudioManager : MonoBehaviour {
 	public AudioClip[] Sounds;
 	//Need to make a way to switch between the states of music
 	//Need them in audio zone so you can hear it for certain things
-	//			ie (Enemy attack, enemy death, portal(when activited))
+	//		ie (Enemy attack, enemy death, portal(when activited))
 	//switching between states for background, death, and powerup
 	//have a state for when hes idle and walking
 	// Use this for initialization
