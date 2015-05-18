e99676921e21843a1c6fb193940de374abe6de67
Garrett Hogan <Garretthogan18@gmail.com>
2015-05-18 09:45:08 -0700

    GameGUI stuff

---
+++ root/Assets/Scripts/Garrett's Scripts/CGameGUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CGameGUI : MonoBehaviour {
+    public float health;
+    [SerializeField]
+    private Text healthText;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        healthText.text = health.ToString();	
+	}
+}
