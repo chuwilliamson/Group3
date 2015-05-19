f4fb69e6c58eee5bd309b02c645f39660221afeb
Garrett Hogan <Garretthogan18@gmail.com>
2015-05-19 08:14:57 -0700

    cgygh

--- root/Assets/Scripts/CMoveToTarget.cs
+++ root/Assets/Scripts/CMoveToTarget.cs
@@ -5,10 +5,13 @@ using System.Collections;
 public class CMoveToTarget : MonoBehaviour 
 {
     public GameObject p;
+    void Awake()
+    {
+        p = GameObject.FindGameObjectWithTag("Player");
+    }
     void Update()
     {
         //Vector3 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
         GetComponent<NavMeshAgent>().destination = p.transform.position;
-        print(p);
     }
 }
