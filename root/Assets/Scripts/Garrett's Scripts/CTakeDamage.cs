66963d720c2f7982e0a4306c71bd3959cfeda3b0
Rundas42 <therisingsun777@yahoo.com>
2015-06-02 16:45:52 +1100

    Health bar, enemy damage, merged into one script

--- root/Assets/Scripts/Garrett's Scripts/CTakeDamage.cs
+++ root/Assets/Scripts/Garrett's Scripts/CTakeDamage.cs
@@ -3,8 +3,15 @@ using System.Collections;
 
 public class CTakeDamage : MonoBehaviour
 {
-
+    public float maxHealth = 100;
     public float health;
+
+    public float healthBarLength;
+
+    void Start()
+    {
+        healthBarLength = Screen.width / 2;
+    }
 
     //character controllers only respond to collisions if they are moving
     //OnCollisionEnter is called when this collider/rigidbody has begun touching another rigidbody/collider.
@@ -22,7 +29,7 @@ public class CTakeDamage : MonoBehaviour
         {
             health -= a_col.transform.gameObject.GetComponent<CProjectile>().damage;
         }
-	
+
         if (this.CompareTag("Player"))
         {
             if (this.CompareTag("Enemy"))
@@ -37,10 +44,9 @@ public class CTakeDamage : MonoBehaviour
             {
                 health -= a_col.gameObject.GetComponent<CEnemy>().attackDamage;
                 print("enemy touched player");
+
             }
         }
-
-
     }
 
     void OnCollisionStay(Collision col)
@@ -64,22 +70,51 @@ public class CTakeDamage : MonoBehaviour
                 GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().SetTrigger("TakeDamage");
             }
         }
-
     }
-
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
+       void FixedUpdate()
+     {
+         adjustCurrentHealth(0);
+         if (health < 0)
+         {
+             if (CompareTag("Player"))
+             {                
+                 GetComponent<Animator>().SetTrigger("ded");
+                 print("set ded");
+             }
+            
+              Destroy(this.gameObject);
+         }
+     }
 
-        if (health < 0)
-        {
-            if (CompareTag("Player"))
-            {                
-                GetComponent<Animator>().SetTrigger("ded");
-                print("set ded");
-            }
-             Destroy(this.gameObject);
-        }
-    }
-}
+       // HealthBar - Seth
+       void OnGUI()
+       {
+           if (this.CompareTag("Player"))
+           {
+ 
+               GUI.Box(new Rect(10, 10, healthBarLength, 20), health + "/" + maxHealth);
+           }
+       }
+       public void adjustCurrentHealth(int adjustment)
+       {
+           if (this.CompareTag("Player"))
+           {
+
+               health += adjustment;
+               if (health < 0)
+               {
+                   health = 0;
+                   Application.LoadLevel("GameOver");
+               }
+               if (health > maxHealth)
+                   health = maxHealth;
+               if (maxHealth < 1)
+                   maxHealth = 1;
+               healthBarLength = (Screen.width / 2) * (health / (float)maxHealth);
+           }
+       }
+    ////////////////
+ }
+     
